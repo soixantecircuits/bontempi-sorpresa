82974f862aa1b8faa34dd4490b167eed2d51fb5f
guillaume-dev <guillaume.rouxel75@gmail.com>
2014-07-29 12:56:16 +0200

    fix native errors

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var express = require('express'),
     fs = require('fs');
 
 mongoose.connect(config.db);
-var db = mongoose.connection;
-db.on('error', function() {
-    throw new Error('unable to connect to database at ' + config.db);
-});
+// var db = mongoose.connection;
+// db.on('error', function() {
+//     throw new Error('unable to connect to database at ' + config.db);
+// });
 
 var modelsPath = __dirname + '/app/models';
 fs.readdirSync(modelsPath).forEach(function(file) {
